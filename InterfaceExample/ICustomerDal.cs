5643af5fbf9175689b3ffe1d90f5bd87f9c7e35b
Enesayyildiz1 <66443194+Enesayyildiz1@users.noreply.github.com>
2020-12-20 22:45:30 +0300

    Add project files.

---
+++ InterfaceExample/ICustomerDal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface ICustomerDal
+    {
+        void Add();
+        void Delete();
+        void Update();
+    }
+    class SqlDatabase : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql Added.");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql Deleted.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Sql Updated.");
+        }
+    }
+    class OracleDatabase : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added.");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Updated.");
+        }
+    }
+    class CustomerManager
+    {
+        public void Do(ICustomerDal customerDal )
+        {
+            customerDal.Add();
+            customerDal.Delete();
+            customerDal.Update();
+        }
+    }
+}
---
+++ InterfaceExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+            Demo();
+            Console.WriteLine("----------------------");
+            ICustomerDal[] customerDals = new ICustomerDal[2]
+            {
+                new SqlDatabase() ,
+                new OracleDatabase()
+            };
+            foreach (var database in customerDals)
+            {
+                database.Add();
+                database.Delete();
+                database.Update();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Do (new SqlDatabase());
+            customerManager.Do (new OracleDatabase());
+           
+           
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+
+            manager.Add(new Student
+            {
+                FirstName = "Enes"
+            });
+            manager.Add(new Customer
+            {
+                FirstName = "Emre"
+            });
+            manager.Add(new Employee
+            {
+                FirstName = "Mahmut"
+            });
+        }
+    }
+}
